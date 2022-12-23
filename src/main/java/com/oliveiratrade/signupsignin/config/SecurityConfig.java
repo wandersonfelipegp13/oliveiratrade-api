83ccbb26d3f1036d1a3d019d05d6caa56f11e733
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-12-23 17:20:55 -0300

    feat: oath2 resource server configuration

--- src/main/java/com/oliveiratrade/signupsignin/config/SecurityConfig.java
+++ src/main/java/com/oliveiratrade/signupsignin/config/SecurityConfig.java
@@ -10,10 +10,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
@@ -39,6 +43,8 @@ public class SecurityConfig {
                         .requestMatchers(toH2Console()).permitAll()
                         .anyRequest().authenticated()
                 )
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .build();
     }
 
