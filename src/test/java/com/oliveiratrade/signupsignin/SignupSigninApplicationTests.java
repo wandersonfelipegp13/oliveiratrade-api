d7ae05aff553f9b321ee8754c824c70b1b95938f
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-12-23 15:53:58 -0300

    Initial structure

---
+++ src/test/java/com/oliveiratrade/signupsignin/SignupSigninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveiratrade.signupsignin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupSigninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
